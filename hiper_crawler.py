1c0c69ed9d4301e033816c2158dcaf379058b7aa
Lucas Leguizamón <107967385+Kalusa93@users.noreply.github.com>
2023-11-10 01:14:58 -0300

    Scrapeando nombres y urls

---
+++ hiper_crawler.py
@@ -0,0 +1,17 @@
+from requests_html import HTMLSession
+
+url = 'https://www.hiperlibertad.com.ar/tecnologia/tv-y-video'
+
+s = HTMLSession()
+r = s.get(url)
+
+r.html.render(sleep=1)
+
+item = r.html.find('.vtex-search-result-3-x-galleryItem')
+
+products = r.html.xpath('//*[@id="gallery-layout-container"]', first=True)
+
+for item in products.absolute_links:
+    r = s.get(item)
+    name = r.html.find('body > div.render-container.render-route-store-product > div > div.vtex-store__template.bg-base > div > div > div > div:nth-child(3) > div > div:nth-child(1) > div > section > div > div > div > div:nth-child(2) > div > div > div.pr0.items-stretch.vtex-flex-layout-0-x-stretchChildrenWidth.flex > div > div:nth-child(3) > h1 > span', first=True).text
+
